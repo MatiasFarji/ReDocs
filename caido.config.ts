a11e77eef8f4cbff17f94b811bcf2fc719989746
Amr Elsagaei <dev@amrelsagaei.com>
2026-06-24 12:02:18 -0300

    Bump Version

--- caido.config.ts
+++ caido.config.ts
@@ -1,18 +1,20 @@
-import { defineConfig } from '@caido-community/dev';
-import vue from '@vitejs/plugin-vue';
+import path from "path";
+
+import tailwindCaido from "@caido/tailwindcss";
+import { defineConfig } from "@caido-community/dev";
+import vue from "@vitejs/plugin-vue";
+import prefixwrap from "postcss-prefixwrap";
 import tailwindcss from "tailwindcss";
 // @ts-expect-error no declared types at this time
 import tailwindPrimeui from "tailwindcss-primeui";
-import tailwindCaido from "@caido/tailwindcss";
-import path from "path";
-import prefixwrap from "postcss-prefixwrap";
 
 const id = "redocs";
 export default defineConfig({
   id,
   name: "ReDocs",
-  description: "Import Postman Collections & OpenAPI Specifications as Replay Sessions",
-  version: "1.1.0",
+  description:
+    "Import Postman Collections & OpenAPI Specifications as Replay Sessions",
+  version: "1.1.1",
   author: {
     name: "Amr Elsagaei",
     email: "info@amrelsagaei.com",
@@ -25,9 +27,9 @@ export default defineConfig({
       root: "packages/backend",
     },
     {
-      kind: 'frontend',
+      kind: "frontend",
       id: "redocs-frontend",
-      root: 'packages/frontend',
+      root: "packages/frontend",
       backend: {
         id: "redocs-backend",
       },
@@ -36,19 +38,19 @@ export default defineConfig({
         build: {
           rollupOptions: {
             external: [
-              '@caido/frontend-sdk', 
-              "@codemirror/state", 
-              "@codemirror/view", 
-              "@codemirror/autocomplete", 
-              "@codemirror/commands", 
-              "@codemirror/lint", 
-              "@codemirror/search", 
-              "@codemirror/language", 
-              "@lezer/common", 
-              "@lezer/highlight", 
-              "@lezer/lr"
-            ]
-          }
+              "@caido/frontend-sdk",
+              "@codemirror/state",
+              "@codemirror/view",
+              "@codemirror/autocomplete",
+              "@codemirror/commands",
+              "@codemirror/lint",
+              "@codemirror/search",
+              "@codemirror/language",
+              "@lezer/common",
+              "@lezer/highlight",
+              "@lezer/lr",
+            ],
+          },
         },
         resolve: {
           alias: [
@@ -70,25 +72,24 @@ export default defineConfig({
                   preflight: false,
                 },
                 content: [
-                  './packages/frontend/src/**/*.{vue,ts}',
-                  './node_modules/@caido/primevue/dist/primevue.mjs'
+                  "./packages/frontend/src/**/*.{vue,ts}",
+                  "./node_modules/@caido/primevue/dist/primevue.mjs",
                 ],
                 // Check the [data-mode="dark"] attribute on the <html> element to determine the mode
                 // This attribute is set in the Caido core application
                 darkMode: ["selector", '[data-mode="dark"]'],
                 plugins: [
-
                   // This plugin injects the necessary Tailwind classes for PrimeVue components
                   tailwindPrimeui,
 
                   // This plugin injects the necessary Tailwind classes for the Caido theme
                   tailwindCaido,
                 ],
-              })
-            ]
-          }
-        }
-      }
-    }
-  ]
+              }),
+            ],
+          },
+        },
+      },
+    },
+  ],
 });
